f31ce137d8a5af9473dce75166a341df2a5fe10c
Francisco Giordano <frangio.1@gmail.com>
2020-11-27 11:41:24 -0300

    Add example setup with Rollup (#4)
    * Add example setup with Rollup
    
    * Set compact: true in rollup json plugin
    
    Co-authored-by: Santiago Palladino <santiago@openzeppelin.com>
    
    * Add autotask dependencies
    
    * Add web3 external
    
    Co-authored-by: Santiago Palladino <santiago@openzeppelin.com>

---
+++ rollup/rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import builtins from 'builtin-modules';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve({ preferBuiltins: true }),
+    commonjs(),
+    json({ compact: true }),
+    typescript(),
+  ],
+  external: [
+    ...builtins,
+    'ethers',
+    'web3',
+    /^defender-relay-client(\/.*)?$/,
+  ],
+};
