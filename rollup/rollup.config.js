bb8cde258de76f293651367107be5466b8cf966f
sc0vu <alk03073135@gmail.com>
2023-04-19 17:05:31 +0800

    feature: add terser module to compress the output file

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -3,12 +3,16 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import builtins from 'builtin-modules';
+import terser from '@rollup/plugin-terser';
 
+// "node:" protocol
+const nodePrefixedBuiltins = builtins.concat(builtins.map(x => 'node:' + x));
 export default {
   input: 'src/index.ts',
   output: {
     file: 'dist/index.js',
     format: 'cjs',
+    plugins: [terser()],
   },
   plugins: [
     resolve({ preferBuiltins: true }),
@@ -17,7 +21,7 @@ export default {
     typescript(),
   ],
   external: [
-    ...builtins,
+    ...nodePrefixedBuiltins,
     'ethers',
     'web3',
     'axios',
